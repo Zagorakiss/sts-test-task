7748dc149ed1b1503678f1b4a1079451b4e05f41
Zagora <zagorakom@gmail.com>
2018-10-30 05:14:42 +0300

    Updates

---
+++ precache-manifest.f67090a1385794689ca7e2c557b542be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e3bc950c2e14ff108c6",
+    "url": "/sts-test-task/static/js/runtime~main.0e3bc950.js"
+  },
+  {
+    "revision": "f23298b8536a3da51d87",
+    "url": "/sts-test-task/static/js/main.f23298b8.chunk.js"
+  },
+  {
+    "revision": "6c1117db1b2083a86eb4",
+    "url": "/sts-test-task/static/js/1.6c1117db.chunk.js"
+  },
+  {
+    "revision": "f23298b8536a3da51d87",
+    "url": "/sts-test-task/static/css/main.5db5e22a.chunk.css"
+  },
+  {
+    "revision": "fc0f1b39e767c0b12a5c88ca60ec0025",
+    "url": "/sts-test-task/index.html"
+  }
+];
